61e4ae3552a105c679daaf91407451dbda3a32f6
ASHRITA JAMPANA <ASHRITA JAMPANA@192.168.11.204>
2020-03-02 11:11:37 +0530

    My first program after learning usage of git with eclipse.

---
+++ SampleProject/src/main/java/com/epam/SampleProject/SumOfDigits.java
@@ -0,0 +1,11 @@
+package com.epam.SampleProject;
+
+public class SumOfDigits {
+	public static void main(String[] args) {
+		int a, b, c;
+		a = 7;
+		b = 9;
+		c = a + b;
+	System.out.println(c);
+}
+}
